e33a1d0bd0acea07b1f84eb45efcd73b062eeb31
tungvl99ql <hd981loz@gmail.com>
2019-10-03 12:53:37 +0700

    z

---
+++ Assets/buttonlight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonlight : MonoBehaviour
+{
+    private bool on = true;
+    
+ 
+   public void onofflight()
+    {
+        if (on)
+        {
+            GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("light");
+            foreach (GameObject go in gameObjectArray)
+            {
+                go.SetActive(false);
+            }
+            GameObject.Find("Button").GetComponentInChildren<Text>().text = "OFF";
+            on = false;
+        }
+        else
+        {
+            GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("light");
+            foreach (GameObject go in gameObjectArray)
+            {
+                go.SetActive(true);
+            }
+            GameObject.Find("Button").GetComponentInChildren<Text>().text = "ON";
+            on = true;
+        }
+    }
+}
---
+++ Assets/lightcontrol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightcontrol : MonoBehaviour
+{
+    public static lightcontrol instance;
+    private void Awake()
+    {
+        MakeInstance();
+    }
+  
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+}
