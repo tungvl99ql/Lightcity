f947338e5f05b7578c11eeddc20c4ad194d7a6d7
TruongDev199x <truongdev199x@gmail.com>
2019-10-03 13:41:15 +0700

    New Version

--- Assets/buttonlight.cs
+++ Assets/buttonlight.cs
@@ -6,28 +6,33 @@ using UnityEngine.UI;
 public class buttonlight : MonoBehaviour
 {
     private bool on = true;
-    
- 
-   public void onofflight()
+    GameObject[] gameObjectArray;
+    private GameObject button;
+
+    public void Start()
+    {
+        gameObjectArray = GameObject.FindGameObjectsWithTag("light");
+        button = GameObject.Find("Button");
+    }
+
+    public void onofflight()
     {
         if (on)
         {
-            GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("light");
             foreach (GameObject go in gameObjectArray)
             {
                 go.SetActive(false);
             }
-            GameObject.Find("Button").GetComponentInChildren<Text>().text = "OFF";
+            button.GetComponentInChildren<Text>().text = "OFF";
             on = false;
         }
         else
         {
-            GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("light");
             foreach (GameObject go in gameObjectArray)
             {
                 go.SetActive(true);
             }
-            GameObject.Find("Button").GetComponentInChildren<Text>().text = "ON";
+            button.GetComponentInChildren<Text>().text = "ON";
             on = true;
         }
     }
